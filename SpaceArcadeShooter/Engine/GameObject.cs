7382ed46f1aab041afd1ebd46bf893bc58349d54
sholev <asen.sholev@gmail.com>
2015-09-29 23:17:59 +0300

    Initial project structure

---
+++ SpaceArcadeShooter/Engine/GameObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class GameObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Appear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disappear()
+        {
+            throw new NotImplementedException();
+        }
+    }    
+}
