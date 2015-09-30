3e450291ebf9a89dfe8cbc18d624e17dcafdcc51
niki2552 <pe_ni_ne_vd@abv.bg>
2015-09-30 17:40:17 +0300

    Dvijenie
    Na lqvo i dqsno

--- SpaceArcadeShooter/SpaceArcadeShooter/SpaceArcadeShooter.Designer.cs
+++ SpaceArcadeShooter/SpaceArcadeShooter/SpaceArcadeShooter.Designer.cs
@@ -29,11 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Plane = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Plane
+            // 
+            this.Plane.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Plane.Location = new System.Drawing.Point(93, 163);
+            this.Plane.Name = "Plane";
+            this.Plane.Size = new System.Drawing.Size(70, 67);
+            this.Plane.TabIndex = 0;
+            this.Plane.Text = "Plane";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SpaceArcadeShooter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Plane);
+            this.Name = "SpaceArcadeShooter";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SpaceArcadeShooter_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SpaceArcadeShooter_KeyUp);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Plane;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- SpaceArcadeShooter/SpaceArcadeShooter/SpaceArcadeShooter.cs
+++ SpaceArcadeShooter/SpaceArcadeShooter/SpaceArcadeShooter.cs
@@ -12,9 +12,56 @@ namespace SpaceArcadeShooter
 {
     public partial class SpaceArcadeShooter : Form
     {
+
+        bool right;
+        bool left;
+
+
+
         public SpaceArcadeShooter()
         {
             InitializeComponent();
         }
+
+  
+
+        private void SpaceArcadeShooter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Right)
+            {
+                right = true; 
+            }
+            if (e.KeyCode==Keys.Left)
+            {
+                left = true;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (right==true)
+            {
+                Plane.Left += 5;
+            }
+            if (left == true)
+            {
+                Plane.Left -= 5;
+            }
+        }
+
+        private void SpaceArcadeShooter_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Right)
+            {
+                right = false;
+            }
+            if (e.KeyCode==Keys.Left)
+            {
+                left = false;
+            }
+        }
+
+        
     }
 }
